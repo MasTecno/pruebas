7d39497d6c701fb784d5f7d9ffd3d87c6eccca0c
Cristhoper Vargas <112445661+crisss35@users.noreply.github.com>
2025-08-11 10:30:14 -0400

    Creando migracion central

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,13 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create("users", function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string("uuid", 50);
+            $table->string('nombre');
+            $table->string("alias");
+            $table->string("base");
+            $table->string("usuario");
             $table->string('password');
-            $table->rememberToken();
+            $table->string("estado");
             $table->timestamps();
         });
 
